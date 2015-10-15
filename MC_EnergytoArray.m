17ce63d71e239d974c94fd23d577e2c02718a7e8
unknown <schrenko@E0403-PC23.inet.dkfz-heidelberg.de>
2015-10-15 17:53:07 +0200

    update

--- MC_EnergytoArray.m
+++ MC_EnergytoArray.m
@@ -22,7 +22,7 @@ function [MCdose,MCuct] = MC_EnergytoArray(NAME3ddose,CT,CST,PLAN,scalDOSE,limit
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 filename1 = [NAME3ddose '.3ddose'];
-filename2 = 'Schweinelunge_20150610_BIXEL.density'; %[NAME3ddose '.density'];
+filename2 = [NAME3ddose '.density'];
 
 binFile = fopen(filename2);
 
@@ -38,7 +38,7 @@ MC3ddose.Z = fread(binFile,[1],'int32');
 %Iso =[21.72, 26.79, -42.32];  
 %Iso =[25.04, 17.38, -24.63];  
 %Iso =[24.26, 19.90, -23.99];  
-%dicom.voxelISOcenter = MC_getIsoCenter(CST,CT,0);
+dicom.voxelISOcenter = MC_getIsoCenter(CST,CT,0);
 
 %% get values of 3ddose file
 
@@ -124,7 +124,7 @@ if (round(MC3ddose.resolution(1)) == round(CT.resolution(1)/10))&(round(MC3ddose
   MC3ddose.DoseISO =  MC3ddose.DoseArray(dicom.voxelISOcenter(2),dicom.voxelISOcenter(1),dicom.voxelISOcenter(3));
   
    if boolscal == 0
-   MC3ddose.DoseArray =(MC3ddose.DoseArray/MC3ddose.DoseISO)*scalDOSE;
+   MC3ddose.DoseArray = MC3ddose.DoseArray * 10^18;%(MC3ddose.DoseArray/MC3ddose.DoseISO)*scalDOSE;
    elseif boolscal == 1
    MC3ddose.DoseArray =(MC3ddose.DoseArray/MC3ddose.MAX)*scalDOSE;
    end
